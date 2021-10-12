27af6d3131c495f8cd34968ca4bd6517adbb2772
B10Mid <bryanmccarthy2@gmail.com>
2021-10-12 13:02:32 +0100

    Update exam.c

--- exam.c
+++ exam.c
@@ -1,4 +1,6 @@
 /* This Program runs a secruity authorisation based on numeric codes that are four single-digit numbers between 0-9.  The user can enter a code or generate a random code. After a code is entered/generated, the user can encrypt the code and compare it to a fixed, authorised access code.  After the users code is encrypted, the user has an option to decrypt it.
+
+-> Authorised access code is 1234
  
  Bryan McCarthy
  15/5/20
